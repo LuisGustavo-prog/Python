329698bbed38a90b45d5a7a72009195fbe3159dd
Luis Gustavo Araujo Markovic <>
2025-07-21 15:30:58 -0300

    Update of the exercise
    Creating a mini game

--- desafios/mundo 2/ex53.py
+++ desafios/mundo 2/ex53.py
@@ -1,2 +1,31 @@
-def game(a): # tentar usar o for ao inves do random para ter mais controle do if
+def game(a):
+    import random
+
+    lists = ['pedra', 'papel', 'tesoura']
+    choice = random.choice(lists)
     
+    if a.lower() == 'pedra':
+        if choice == 'tesoura':
+            return 'Você ganhou'
+        elif choice == 'papel':
+            return 'Você perdeu'
+        else:
+            return 'Empate'
+    elif a.lower() == 'papel':
+        if choice == 'pedra':
+            return 'Você ganhou'
+        elif choice == 'tesoura':
+            return 'Você perdeu'
+        else:
+            return 'Empate'
+    elif a.lower() == 'tesoura':
+        if choice == 'papel':
+            return 'Você ganhou'
+        elif choice == 'pedra':
+            return 'Você perdeu'
+        else:
+            return 'Empate'
+    else:
+        return 'Resposta inválida'
+        
+print(game('pedra'))
