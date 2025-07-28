0b70a8cd1a5ba8a8689879dec3547395db7408e5
Luis Gustavo Araujo Markovic <luisgustav743@gmail.com>
2025-07-28 15:02:20 -0300

    Delete def directory

--- def/lista.py
+++
@@ -1,4 +0,0 @@
-lista = ['a', 'b' 'c', 'b', 'a', 'd', 't', 't', 'p', 'p', 'p', 'a', 'p']
-
-print(max(lista, key=lista.len))
-# código pra ver qual letra foi mais usada, pode ser usado com palavras mesmo.
